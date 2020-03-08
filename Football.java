aab74bb1f43aeda9122c25ed2b7b30e4064753e5
sevda <imanysevda@gmail.com>
2020-03-09 02:23:03 +0330

    debug check methods and complete main

--- Football.java
+++ Football.java
@@ -2,36 +2,17 @@ import java.io.*;
 import java.util.*;
 
 class Player {
-    String name;
-    String lastName;
+    String fullName;
 
-    public Player(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public Player(String fullName) {
+        this.fullName = fullName;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
         return result;
     }
 
@@ -44,15 +25,10 @@ class Player {
         if (getClass() != obj.getClass())
             return false;
         Player other = (Player) obj;
-        if (lastName == null) {
-            if (other.lastName != null)
+        if (fullName == null) {
+            if (other.fullName != null)
                 return false;
-        } else if (!lastName.equals(other.lastName))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
+        } else if (!fullName.equals(other.fullName))
             return false;
         return true;
     }
@@ -108,20 +84,23 @@ class Teams {
 
 public class Football {
 
+    // check is all the playrers in team are allowed to play or not
     int check(TeamPlayers singleTeam, ArrayList<Player> allPlayers) {
         Iterator<Player> it = singleTeam.team.iterator();
-        Iterator<Player> it2 = allPlayers.iterator();
+
         int num = 0;
         while (it.hasNext()) {
-
+            Iterator<Player> it2 = allPlayers.iterator();
             Player c = it.next();
             while (it2.hasNext()) {
-                if (c.equals(it2.next())) {
+                boolean a = c.equals(it2.next());
+                if (a) {
                     num = 1;
+                    break;
                 }
             }
             if (num == 0) {
-                return -1;
+                return 1;
             }
 
         }
@@ -129,22 +108,27 @@ public class Football {
 
     }
 
-    int check2(TeamPlayers singleTeam, Teams allTeams) {
+    // check that each player just be in one team
+    int check2(TeamPlayers singleTeam, Teams allTeams, ArrayList<Integer> eachTeamPlayerNum) {
         Iterator<TeamPlayers> it = allTeams.teams.iterator();
-        Iterator<Player> itPlayer = singleTeam.team.iterator();
+        Iterator<Integer> it2 = eachTeamPlayerNum.iterator();
         while (it.hasNext()) {
-            if (singleTeam.equals(it.next()))
+            
+            int j = it2.next();
+            TeamPlayers teamPlayers = it.next();
+            if (singleTeam.equals(teamPlayers))
                 continue;
             else {
-                for (int i = 0; i < allTeams.teams.size(); i++) {
+                for (int i = 0; i < j; i++) {
+                    Iterator<Player> itPlayer = singleTeam.team.iterator();
                     while (itPlayer.hasNext()) {
-                        if (itPlayer.next().equals(allTeams.teams.get(i))) {
-                            return -1;
+                        if (itPlayer.next().equals(teamPlayers.team.get(i))) {
+                            return 1;
                         }
                     }
                 }
             }
-
+            j++;
         }
         return 0;
     }
@@ -153,6 +137,8 @@ public class Football {
         int numTeam;
         int numPlayers;
         ArrayList<Player> allPlayers = new ArrayList<Player>();
+        // arraylist for keeping teams player num
+        ArrayList<Integer> eachTeamPlayerNum = new ArrayList<Integer>();
         Teams allTeams = new Teams();
         try {
             File x = new File("input.txt");
@@ -162,28 +148,55 @@ public class Football {
             numTeam = sc.nextInt();
 
             // read and add all players
+            sc.nextLine();
             for (int i = 0; i < numPlayers; i++) {
-                String name = sc.next();
-                String lastName = sc.next();
-                Player singlePlayer = new Player(name, lastName);
+                String name = sc.nextLine();
+                Player singlePlayer = new Player(name);
                 allPlayers.add(singlePlayer);
+
             }
 
             // read and add players in their team
             for (int i = 0; i < numTeam; i++) {
+                // read team name
                 String teamName = sc.nextLine();
                 TeamPlayers team = new TeamPlayers(teamName);
                 allTeams.addTeams(team);
                 int numTeamPlayers = sc.nextInt();
+                eachTeamPlayerNum.add(numTeamPlayers);
+                sc.nextLine();
+                // read team players name
                 for (int j = 0; j < numTeamPlayers; j++) {
-                    String name = sc.next();
-                    String lastName = sc.next();
-                    Player singlePlayer = new Player(name, lastName);
+                    String name = sc.nextLine();
+                    Player singlePlayer = new Player(name);
                     team.addPlayer(singlePlayer);
                 }
             }
 
-        
+            sc.close();
+            Iterator<TeamPlayers> itTeamPlayer = allTeams.teams.iterator();
+            Football footballcheck = new Football();
+
+            // new an arrayList for putting final teams name to print
+            ArrayList<String> chosenTeams = new ArrayList<String>();
+
+            while (itTeamPlayer.hasNext()) {
+                TeamPlayers teamPlayerExample = itTeamPlayer.next();
+                int a = footballcheck.check(teamPlayerExample, allPlayers);
+                int b = footballcheck.check2(teamPlayerExample, allTeams, eachTeamPlayerNum);
+                if ((a + b) != 0) {
+                    chosenTeams.add(teamPlayerExample.name);
+                }
+
+            }
+            // sort the final teams list
+            Collections.sort(chosenTeams);
+            // print final teams name
+            Iterator<String> it = chosenTeams.iterator();
+            while (it.hasNext()) {
+                System.out.println(it.next());
+            }
+
         } catch (FileNotFoundException e) {
             System.out.println("cannot open the File!");
         }
